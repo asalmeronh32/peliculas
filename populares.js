2742f54f841e3bb9d5efbaad6c73b4b1af91ebdb
asalmeronh32 <asalmeronh@alumnes.santjosepobrer.es>
2022-04-05 02:52:12 +0200

    copiado

---
+++ populares.js
@@ -0,0 +1,32 @@
+let pagina = 1;
+let peliculas = '';
+
+const Peliculas = async() => {
+	try {
+		const respuesta = await axios.get('https://api.themoviedb.org/3/movie/popular', {
+			params: {
+				language: 'es-ES',
+				page: pagina
+			},
+			headers: {
+				'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxOTJlMGI5ODIxNTY0ZjI2ZjUyOTQ5NzU4ZWEzYzQ3MyIsInN1YiI6IjYxODQyMWZlOGVkMDNmMDAyZDA4ZjZlZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nrCdKCx2dZQ7d0WaJpXJaPO_De2iP2rYg9bPon1O3V0'
+			}
+		})
+		if(respuesta.status === 200){
+			respuesta.data.results.forEach(pelicula => {
+				peliculas += `
+				<div class="pelicula">
+				<a href="https://www.themoviedb.org/movie/${pelicula.id}-${pelicula.title}"><img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}"></a>
+					 <h3 class="titulo">${pelicula.title}</h3>
+				</div>
+				`;
+			});
+			document.getElementById('contenedor').innerHTML = peliculas;
+		}
+
+	} catch(error){
+		console.log(error);
+	}
+
+}
+Peliculas();
